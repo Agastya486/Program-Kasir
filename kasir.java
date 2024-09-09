c738501bf3ff217a99323aaa1b4bf5030bd2d1be
Agastya <123287468+Agastya486@users.noreply.github.com>
2024-09-09 23:02:12 +0700

    Add files via upload

---
+++ kasir.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class kasir{
+        public static void main(String[] args){
+                Scanner input = new Scanner(System.in);
+
+                int hargaPerKG;
+                double jumlahBerasKG, hargaAsli, hargaDiskon, nilaiDiskon, uang, kembalian;
+
+     
+                hargaPerKG = 14000;
+                System.out.println("Harga Beras per KG : " + hargaPerKG);
+
+                System.out.print("Masukkan jumlah beras dalam KG : ");
+                jumlahBerasKG = input.nextDouble();
+
+                hargaAsli = hargaPerKG * jumlahBerasKG;
+                System.out.println("Harga asli : " + hargaAsli);
+
+                nilaiDiskon = hargaAsli * 0.05;
+
+                hargaDiskon = hargaAsli - nilaiDiskon;
+                System.out.println("Harga setelah diskon 5% : " + hargaDiskon);
+
+
+
+                System.out.print("Masukkan uang anda : ");
+                uang = input.nextDouble();
+                System.out.println("Uang anda : " + uang);
+
+                kembalian = uang - hargaDiskon;
+                System.out.println("Kembalian anda : " + kembalian);
+        }
+}
